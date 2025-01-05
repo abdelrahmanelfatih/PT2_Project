8e6f34cf17e5a3f8e6ec8df338c8034b920828f7
nar7g <NAR7GAMER@gmial.com>
2025-01-05 16:51:27 +0800

    skibidi sigma

--- Location.h
+++ Location.h
@@ -1,7 +1,9 @@
 #ifndef LOCATION_H
 #define LOCATION_H
 #include <string>
+#include <map>
 using std::string;
+using std::map;
 
 class Location{
     string locationID;
@@ -14,9 +16,9 @@ public:
     Location(string id, string name, string address, int capacity, string status) :
     locationID(id), locationName(name), address(address), capacity(capacity), status(status) {}
 
-    void addLocation(string name, string address, string capacity);
-    void updateLocation(string id, string name, string address, int capacity, string status);
-    Location getLocationDetails(string id);
+    void addLocation(string name, string address, int capacity);
+    void updateLocation(string id = "", string name = "",string address = "", int capacity = 0, string status = "");
+    map<string, string> getLocationDetails() const;
 };
 
 
