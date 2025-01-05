58ef5949d256bd3b124fe506be611b6910b32c41
Mukhtar Mohamed <toojywasoney360@gmail.com>
2025-01-05 17:20:11 +0800

    Add files via upload

---
+++ REVIEW.cpp
@@ -0,0 +1,17 @@
+#include "Review.h"
+
+const string& Review::submitReview(string& id, string& name, int rate, string& comm) const {
+  
+    return "Review Submitted!"; 
+}
+
+bool Review::deleteReview(string reviewID) {
+    // delete the review by its ID yuh
+    return true; // true if deletion is successful, false if not
+}
+
+double Review::getAverageRating(string eventID) {
+    // calculate and return the average rating for the event identified by eventID
+  
+    return 0.0; // <--- Placeholder for the actual calculated average
+}
