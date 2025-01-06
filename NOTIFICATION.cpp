d88f0bad2f46b73bac44e6a2041e9c99e9485f15
Mukhtar Mohamed <toojywasoney360@gmail.com>
2025-01-06 22:24:25 +0800

    Updated NOTIFICATION.cpp

--- NOTIFICATION.cpp
+++ NOTIFICATION.cpp
@@ -1,9 +1,16 @@
 #include "Notification.h"
 
+Notification::Notification(const string& id, const string& msg, const string& time)
+    : notificationID(id), message(msg), timeStamp(time) {}
+
 string Notification::sendMessage() const {
     return "Notification sent: " + message;
 }
 
-void Notification::setMessage(string& msg) {
+void Notification::setMessage(const string& msg) {
     message = msg;
 }
+
+string Notification::getNotificationID() const { return notificationID; }
+string Notification::getMessage() const { return message; }
+string Notification::getTimeStamp() const { return timeStamp; }
