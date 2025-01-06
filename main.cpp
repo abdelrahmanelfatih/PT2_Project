c35b56e5e64c1d5a33db86a2e160454de5fe3a1e
Hamza Zacaria Mohammad <166973618+bamuzo@users.noreply.github.com>
2025-01-06 21:15:35 +0800

    Update main.cpp
    added ticket.hpp and event coordinator.hpp for the testing environment  as well as the testing stuff

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "Resource.hpp"
 #include "Review.h"
 #include "Notification.h"
+#include "EVENT_COORDINATOR.hpp"
+#include "TICKET.hpp"
 
 using namespace std;
 
@@ -63,6 +65,67 @@ int main() {
     cout << "Calculating Average Rating..." << endl;
     double averageRating = review.getAverageRating(eventID);
     cout << "Average Rating for Event " << eventID << ": " << averageRating << endl;
+
+    // Event Coordinator Test
+
+    map<string, EventCoordinator> coordinators;
+
+    EventCoordinator coord1, coord2;
+    
+    coord1.assignCoordinator("Abdelrahman Elfatih", "Abdul@gmail.com");
+    coord2.assignCoordinator("Aditya Vivaldi", "aditviv@gmail.com");
+
+    coordinators[coord1.getCoordinatorDetails().at("Coordinator ID")] = coord1;
+    coordinators[coord2.getCoordinatorDetails().at("Coordinator ID")] = coord2;
+    
+     for (const auto& entry : coordinators) {
+        cout << "Coordinator ID: " << entry.first << endl;
+
+        
+        for (const auto& detail : entry.second.getCoordinatorDetails()) {
+            if (detail.first != "Coordinator ID") {
+                cout << detail.first << ": " << detail.second << endl;
+            }
+        }
+     }
+
+    // Ticket Test
+
+    Ticket ticket;
+
+    string ticketID = ticket.generateTicket("V10", "VIP", 250.00, "2025-1-6");
+    cout<<"Ticket Generated, Ticket ID: "<< ticketID<<endl;
+
+    if(ticket.validateTicket(ticketID)){
+        cout<<"Valid Ticket"<< endl;
+    } else{
+        cout<<"Invalid ticket"<<endl;
+    }
+
+    try
+    {
+        Ticket details = ticket.getTicketDetails(ticketID);
+        cout << "Ticket Details:" << endl;
+        cout << "ID: " << ticketID << endl;
+        cout << "Price: " << details.getPrice() << endl;
+        cout << "Date: " << details.getDate() << endl;
+    }
+    catch(const std::invalid_argument& e)
+    {
+        cout << "Error: " << e.what() << endl;
+    }
+    
+    if(ticket.refundTicket(ticketID)){
+        cout<<"ticket refunded."<<endl;
+
+    } else{
+        cout<<"refund Failed"<<endl;
+    }
+
+    system("pause");
+
+    return 0;
+}
     
     system("pause");
     return 0;
